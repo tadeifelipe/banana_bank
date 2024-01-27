7043070e0327b2e5c2d378c3f91b2bf4435b8baa
tadeifelipe <felipe_santostadei@hotmail.com>
2024-01-27 11:29:04 -0300

    Crypto password user

--- lib/banana_bank/users/user.ex
+++ lib/banana_bank/users/user.ex
@@ -2,10 +2,11 @@ defmodule BananaBank.Users.User do
   use Ecto.Schema
   import Ecto.Changeset
 
-  @required_params [:name, :password_hash, :email, :cep]
+  @required_params [:name, :password, :email, :cep]
 
   schema "users" do
     field :name, :string
+    field :password, :string, virtual: true
     field :password_hash, :string
     field :email, :string
     field :cep, :string
@@ -15,10 +16,19 @@ defmodule BananaBank.Users.User do
 
   def changeset(user \\ %__MODULE__{}, params) do
     user
-      |> cast(params, @required_params)
-      |> validate_required(@required_params)
-      |> validate_length(:name, min: 3)
-      |> validate_format(:email, ~r/@/)
-      |> validate_length(:cep, min: 8)
+    |> cast(params, @required_params)
+    |> validate_required(@required_params)
+    |> validate_length(:name, min: 3)
+    |> validate_format(:email, ~r/@/)
+    |> validate_length(:cep, min: 8)
+    |> add_password_hash()
   end
+
+  defp add_password_hash(
+         %Ecto.Changeset{valid?: true, changes: %{password: password}} = changeset
+       ) do
+    change(changeset, %{password_hash: Pbkdf2.hash_pwd_salt(password)})
+  end
+
+  defp add_password_hash(changeset), do: changeset
 end
