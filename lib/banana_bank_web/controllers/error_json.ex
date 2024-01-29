e0e6cd30687802fc5798270eba00f95ee7c3aab0
tadeifelipe <felipe_santostadei@hotmail.com>
2024-01-29 07:48:55 -0300

    add tesla client

--- lib/banana_bank_web/controllers/error_json.ex
+++ lib/banana_bank_web/controllers/error_json.ex
@@ -1,32 +1,24 @@
 defmodule BananaBankWeb.ErrorJSON do
-  # If you want to customize a particular status code,
-  # you may add your own clauses, such as:
-  #
-  # def render("500.json", _assigns) do
-  #   %{errors: %{detail: "Internal Server Error"}}
-  # end
-
-  # By default, Phoenix returns the status message from
-  # the template name. For example, "404.json" becomes
-  # "Not Found".
   def render(template, _assigns) do
     %{errors: %{detail: Phoenix.Controller.status_message_from_template(template)}}
   end
 
   def error(%{changeset: changeset}) do
-    # When encoded, the changeset returns its errors
-    # as a JSON object. So we just pass it forward.
     %{
       errors: Ecto.Changeset.traverse_errors(changeset, &translate_errors/1)
     }
   end
 
   def error(%{status: :not_found}) do
-    # When encoded, the changeset returns its errors
-    # as a JSON object. So we just pass it forward.
     %{
       status: :not_found,
-      message: "User not found"
+      message: "Resource not found"
+    }
+  end
+
+  def error(%{status: status}) do
+    %{
+      status: status
     }
   end
 
